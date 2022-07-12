465d0ff60dc508899e09488972feadf1312c66ca
Beto Betin <95883258+betinb623@users.noreply.github.com>
2022-07-12 22:41:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Soma dos multiplos de 3 e 5 de um numero
+
+somar (10);
+function somar(limite) {
+  let multiploDe3 = 0;
+  let multiploDe5 = 0;
+
+  for (i = 0; i <= limite ; i++){
+    if (i % 3 === 0)
+      multiploDe3 += i;
+    if (i % 5 === 0)
+      multiploDe3 += i;
+  }
+  console.log(multiploDe3 + multiploDe5)
+}
